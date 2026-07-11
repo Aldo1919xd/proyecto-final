235b85c9d8875816ff67806f3667fe36ab2da275
Billy <techstudentbilly@gmail.com>
2026-07-10 22:01:24 -0500

    Creación de entidad categoria y agregado de restricción validation

---
+++ src/main/java/com/example/proyecto_final/entity/Categoria.java
@@ -0,0 +1,52 @@
+package com.example.proyecto_final.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codCategoria;
+
+    @NotBlank(message = "El nombre de la categoria es obligatorio")
+    @Size(max = 80, message = "Maximo 80 caracteres permitidos")
+    @Column(nullable = false, length = 80)
+    private String nombreCategoria;
+
+    @Column(nullable = false)
+    private Boolean estado = true;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer codCategoria) {
+        this.codCategoria = codCategoria;
+    }
+
+    public Integer getCodCategoria() {
+        return codCategoria;
+    }
+
+    public void setCodCategoria(Integer codCategoria) {
+        this.codCategoria = codCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
