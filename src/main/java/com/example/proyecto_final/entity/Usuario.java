1b61d9cb5935120764a62311e90a5802631a544f
Ariana <ariana.nikol30@gmail.com>
2026-07-10 21:54:38 -0500

    Eliminar clases y archivos innecesarios: DataInitializer, GlobalControllerAdvice, AuditoriaController y RolController; agregar nuevas entidades: Auditoria, Funcionalidad, Rol y RolFuncionalidad.

--- src/main/java/com/example/proyecto_final/entity/Usuario.java
+++ src/main/java/com/example/proyecto_final/entity/Usuario.java
@@ -2,9 +2,10 @@ package com.example.proyecto_final.entity;
 
 import java.time.LocalDateTime;
 
-import org.antlr.v4.runtime.misc.NotNull;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Usuario")
